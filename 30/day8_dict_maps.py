c8bfb01bfb5baec19faa8f05c0c28fe073a7ce60
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-10 12:20:29 -0800

    [30_days_code] day8 dict/maps

---
+++ 30/day8_dict_maps.py
@@ -0,0 +1,36 @@
+import time
+
+
+def find_key(dictionary):
+    '''
+        Returns key and value if key is exist in dictionary else 
+        not found. 
+    '''
+    while True: # takes as much as input until there is no input 
+        try:
+            name = input()
+            if name in dictionary:
+                print("{}={}".format(name, dictionary[name]))
+            else:
+                print("Not found")
+        except:
+            break
+    
+    
+        
+
+  
+  
+if __name__ == "__main__":
+    start_time = time.time()
+
+
+    dict_lenght = int(input())
+    phone_book = dict(input().split() for _ in range(dict_lenght))
+    find_key(phone_book)
+    
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+    
