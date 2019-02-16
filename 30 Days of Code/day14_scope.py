4063f0af77682c7014d698843374948f8f91a790
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-15 20:40:41 -0800

    [day_14&day_16] scope and exception

---
+++ 30 Days of Code/day14_scope.py
@@ -0,0 +1,18 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    def computeDifference(self):
+        """Compute the max difference of two elements from the array"""
+        
+        self.maximumDifference = (max(self.__elements) - min(self.__elements))
+
+
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
