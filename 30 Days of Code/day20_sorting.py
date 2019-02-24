a9b17fd48470c39f0e15688537cfb8d0e2fe8b69
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-23 19:46:22 -0800

    [optimized] bubble sorting

--- 30 Days of Code/day20_sorting.py
+++ 30 Days of Code/day20_sorting.py
@@ -1,3 +1,22 @@
+import time
+def optimized_bubble_sort(arr):
+    """
+        Sorts the array with bubble sort algorithm
+        and returns number of swaps made to sort the array
+    """
+    count = 0
+    swapped = False
+    while(swapped != True):
+        swapped = True
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                count += 1
+                swapped = False
+
+    print(arr)
+    return count
+
 def bubble_sort(arr):
     """
         Sorts the array with bubble sort algorithm
@@ -12,7 +31,10 @@ def bubble_sort(arr):
 
     print(arr)
     return count
-
-
-arr = [4, 1, 3, 5, 2]
-print(bubble_sort(arr))
+if __name__ == "__main__":
+    start_time = time.time()
+    arr = [4, 1, 3, 5, 2]
+    sorted_arr = [x for x in range(10000)]
+    print(optimized_bubble_sort(sorted_arr))
+    # print(sorted_arr)
+    print(f"----{time.time() - start_time} seconds-----")
