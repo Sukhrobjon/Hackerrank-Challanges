42e0a6e1e7c644b88efbd7e3dc40c0b6e30fa5ab
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-03-12 21:13:51 -0700

    [added] is_prime with some optimization

---
+++ ProblemSolving/prime_number.py
@@ -0,0 +1,48 @@
+import time
+import math
+
+
+def is_prime_v1(n):
+    if n == 1:
+        return False
+
+    for devisor in range(2, n):
+        if n % devisor == 0:
+            return False
+    return True
+
+
+def is_prime_v2(n):
+    if n == 1:
+        return False
+    max_divisor = math.floor(math.sqrt(n))
+    for divisor in range(2, 1 + max_divisor):
+        if n % divisor == 0:
+            return False
+    return True
+
+
+def is_prime_v3(n):
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
+
+    max_divisor = math.floor(math.sqrt(n))
+    for divisor in range(3, 1 + max_divisor, 2):
+        if n % divisor == 0:
+            return False
+    return True
+
+
+# ====== Test Function =====
+start_time = time.time()
+for n in range(1, 100000):
+    is_prime_v3(n)
+end_time = time.time()
+print("Time required: ", end_time - start_time)
+
+# for n in range(1, 21):
+#     print(n, is_prime_v2(n))
