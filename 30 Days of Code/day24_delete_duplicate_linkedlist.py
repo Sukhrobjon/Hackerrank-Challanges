167a28cea8de3f641ac8cce4c69d05db835e1774
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-05-28 14:27:57 -0700

    [updated] the remove duplicate in linkedlist

--- 30 Days of Code/day24_delete_duplicate_linkedlist.py
+++ 30 Days of Code/day24_delete_duplicate_linkedlist.py
@@ -24,18 +24,43 @@ class Solution:
             print(current.data, end=' ')
             current = current.next
 
-    # def removeDuplicates(self, head):
-        
-    #     pass
-    def removeDups(self, head):
-        current = second = head
-        while current is not None:
-            while second.next is not None:   # check second.next here rather than second
-                if second.next.data == current.data:   # check second.next.data, not second.data
-                    second.next = second.next.next   # cut second.next out of the list
-                else:
-                    second = second.next   # put this line in an else, to avoid skipping items
-            current = second = current.next
+    def removeDuplicates(self, head):
+        """
+            Remove duplicate elements and return the linked list head
+        """
+        # check if linked list is empty
+        if(head == None):
+            return None
+        # set to hold unique values
+        unique_values = set()
+        # start at the head
+        current = head
+        # keep track of previous node we can set it to either None or head
+        prev = None
+        # traverse through the linked list until current node is none
+        while(current):
+            # if the current node data is in unique values set
+            if(current.data in unique_values):
+                # we want to skip that duplicated value and set
+                # the next poitnter of previous node to current.next
+                prev.next = current.next
+            # if we see this value first time we add it to set
+            else:
+                unique_values.add(current.data)
+                # set the prev to current.
+                prev = current
+            # now basically shift the current node to the next one
+            current = prev.next
+        return head
+    # def removeDups(self, head):
+    #     current = second = head
+    #     while current is not None:
+    #         while second.next is not None:   # check second.next here rather than second
+    #             if second.next.data == current.data:   # check second.next.data, not second.data
+    #                 second.next = second.next.next   # cut second.next out of the list
+    #             else:
+    #                 second = second.next   # put this line in an else, to avoid skipping items
+    #         current = second = current.next
 
 
 def remove_duplicate(arr):
