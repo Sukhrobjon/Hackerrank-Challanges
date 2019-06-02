9db9374c96c0b29f48168dcad72f81192120aaf9
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-01 22:37:55 -0700

    [solved] single number problem

--- capitalize.py
+++ capitalize.py
@@ -1,23 +1,4 @@
 '''
-You are asked to ensure that the first and last names of people begin with 
-a capital letter in their passports. For example, alison heck should be capitalised 
-correctly as Alison Heck.
-
-
-Given a full name, your task is to capitalize the name appropriately.
-
-Input Format
-
-A single line of input containing the full name, .
-
-Constraints
-
-The string consists of alphanumeric characters and spaces.
-Note: in a word only the first character is capitalized. Example 12abc when
-capitalized remains 12abc.
-
-Output Format
-
 Print the capitalized string, .
 
 Sample Input
@@ -39,4 +20,5 @@ def solve(s):
 
 if __name__ == '__main__':
     name = 'chris alan'
-    print(solve(name))
+    print(name.title())
+    # print(solve(name))
--- my_tests.py
+++ my_tests.py
@@ -1,11 +1,22 @@
 
-print(len(set([str(input()) for x in range(int(input()))])))
-a = set([int(i) for i in input().split()])
-b = set([int(i) for i in input().split()])
+# print(len(set([str(input()) for x in range(int(input()))])))
+# a = set([int(i) for i in input().split()])
+# b = set([int(i) for i in input().split()])
 
-# print(len(a.union(b)))
-n = int(input("enter number between 1-20: "))
-k = [x*x for x in range(1,n+1)]
-# for i in range(1,6):
-#     print(i*i)
-print(*k, sep="\n")
+# # print(len(a.union(b)))
+# n = int(input("enter number between 1-20: "))
+# k = [x*x for x in range(1,n+1)]
+# # for i in range(1,6):
+# #     print(i*i)
+# print(*k, sep="\n")
+
+# 2)**Single Number **
+# Given an array of integers, every element appears twice except for one. Find that single one.
+# Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+from collections import Counter
+arr = [1, 2, 4, 5, 1, 4, 5]
+
+arr_d = Counter(arr)
+print(arr_d)
+print(min(arr_d.values()))
