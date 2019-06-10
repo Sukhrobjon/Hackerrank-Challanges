74c47a2e6c9f10e29c8a95deb9995d8d38bb87a2
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-09 21:10:20 -0700

    [finished] longest substring problem

--- two_longest_substring.py
+++ two_longest_substring.py
@@ -1,37 +1,52 @@
 def longest_substring(word):
     longest = 0
     counter = 0
-    seen = set()
+    seen = []
     start = 0
     index = 0
-    
-    while(index < (len(word) - 1)):
+    all_substring = []
+    while index <  len(word):
         # print('index: ', char)
-        print(seen)
+        print("seen: ",seen)
         if word[index] not in seen:
             print("inside if")
-            seen.add(word[index])
+            seen.append(word[index])
 
             counter += 1
-            print(word[index])
-        else:
-            break
-        #     # if counter > longest:
-        #     #     longest = counter
-        #     # counter = 0
-        #     # start += 1
-            # index = start
-            # print(word[:counter])
+            print("current char: ",word[index])
+            print("index: ", index)
+            index += 1
             
-
-    return counter
+        
+        else: # repeated char found
+            print("else")
+            # break
+            all_substring.append(list(seen))
+            if counter > longest: # check if current substring is longest
+                longest = counter
+                print("counter: ", counter)
+            counter = 0 # reset the counter
+            start += 1 # shift the pointer to the next char 
+            index = start # reset the index at the beginning of new substring
+            print("index in else:", index)
+            all_combos = list(seen)
+            print("combo: ", all_combos)
+            seen = [] # reset the set for new possible substring 
+            # print(word[:counter])
+        print('last set:', seen)
+        # index += 1
+    print(all_substring)
+    return longest
 
 # i = 0
 # while i < 9:
 #     print("i: ", i)
 #     i += 1
 
-a = 'ihb'
+
+a = 'pwwkew'
+b = 'abcabcbb'
+c = 'bbbbb'
 print(longest_substring(a))
 
 
