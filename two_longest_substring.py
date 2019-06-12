3f9e9b2de69adf8555e13047f5401ea6b9d1a639
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-11 17:59:54 -0700

    [optimized] the longest substring

--- two_longest_substring.py
+++ two_longest_substring.py
@@ -63,24 +63,45 @@ class Solution(object):
 
         longest = 0
         counter = 0
-        seen = []
-        start = 0
+        seen = set()
+        # start = 0
         index = 0
 
-        while index < len(word):
+        while index < len(word) - 1:
 
             if word[index] not in seen:  # check if the char not in the list
-                seen.append(word[index])
+                seen.add(word[index])
                 counter += 1
-                index += 1
-
             else:  # repeated char found
-
-                counter = 0  # reset the counter
-                start += 1  # shift the pointer to the next char
-                index = start  # reset the index at the beginning of new substring
-                seen = []  # reset the set for new possible substring
+                seen.add(word[index])  # empty the set and add the current char
+                counter = 1 # and add one to counter bc there is one char in the set already
+            
             if counter > longest:  # check if current substring is longest
                     longest = counter
+            index += 1
+        return longest
+
+    def longest_substring(self, word):
+        longest = 0
+        seen = set()
+        counter = 0
+        for char in word:
+            if char not in seen:
+                seen.add(char)
+                counter += 1
+            else:
+                seen = set(char)
+                counter = 1
+            
+            if counter > longest:
+                longest = counter
 
         return longest
+
+
+
+a = 'abcabcbb'
+b = 'pwwkew'
+c = "dvdf"
+obj = Solution()
+print(obj.longest_substring(a))
