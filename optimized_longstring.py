869a733597a14e345a79763ab667c7b643b548d1
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-12 12:48:05 -0700

    [code] up the SPD interview problems

--- optimized_longstring.py
+++ optimized_longstring.py
@@ -1,1 +1,36 @@
 # follow what Alan has offered
+
+'''
+Ikey's solution for longest unique substring
+
+def longest_unique_substring(str):
+    longest = ""
+    curr = ""
+
+    for char in str:
+        if char in curr:
+            if len(curr) > len(longest):
+                longest = curr
+
+            index_of_dub = curr.index(char)
+            curr = curr[index_of_dub + 1:]
+
+        curr += char
+
+    return longest
+
+a = "abcabcbb"
+
+print(longest_unique_substring(a))'''
+
+
+def capitalized_words(arr):
+    capitalized = []
+
+    for word in arr:
+        if word[0].isupper():
+            capitalized.append(word)
+
+    return capitalized
+a = ['APp and', 'Python', 'javascpript']
+print(capitalized_words(a))
