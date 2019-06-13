1a9e3f7b1eaf2a77209da18834f1c6e4f4091a3d
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-12 18:28:50 -0700

    [worked] on longest substring using dictionary

--- optimized_longstring.py
+++ optimized_longstring.py
@@ -23,6 +23,50 @@ a = "abcabcbb"
 
 print(longest_unique_substring(a))'''
 
+def optimized_longest_substring(word):
+    seen = {}
+    index = starter = longest = counter = 0
+
+    while index < len(word) - 1:
+        
+        cur_char = word[index]
+        
+        if cur_char not in seen:    
+            seen[cur_char] = index + 1
+            print("index in if: ", index)
+            index += 1
+            
+
+        else: # see the duplicate
+            # print(cur_char)
+            # before setting the new index of the char we need to shift the starter
+            starter = seen[cur_char]
+            
+            # set the dublicated char's value with updated index
+            seen[cur_char] = index + 1
+            # updatet the counter
+            counter = index - starter + 1
+            print("counter: ", counter)
+            print("starter: {}, index: {}".format(starter, index))
+            # reset the index to start at the starter
+            index = starter
+            print("index after new starter: ", index)
+            
+            
+        longest = max(counter, longest)
+        print("longest: ",longest)
+        print(seen)
+    return longest
+
+
+a = 'abcabcbb'
+b = 'dvdf'
+print(optimized_longest_substring(b))
+
+
+
+
+
 
 def capitalized_words(arr):
     capitalized = []
@@ -33,4 +77,5 @@ def capitalized_words(arr):
 
     return capitalized
 a = ['APp and', 'Python', 'javascpript']
-print(capitalized_words(a))
+# print(capitalized_words(a))
+
