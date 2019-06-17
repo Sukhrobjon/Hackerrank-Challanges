d9d7fe18df1952198a53a78eaeb489e0d975d034
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-17 09:15:20 -0700

    [removed] duplicated code and [organized] files

--- optimized_longstring.py
+++ optimized_longstring.py
@@ -1,73 +1,35 @@
-# follow what Alan has offered
-
-'''
-Ikey's solution for longest unique substring
-
-def longest_unique_substring(str):
-    longest = ""
-    curr = ""
-
-    for char in str:
-        if char in curr:
-            if len(curr) > len(longest):
-                longest = curr
-
-            index_of_dub = curr.index(char)
-            curr = curr[index_of_dub + 1:]
-
-        curr += char
-
-    return longest
-
-a = "abcabcbb"
-
-print(longest_unique_substring(a))'''
 
 def optimized_longest_substring(word):
     seen = {}
-    index = starter = longest = counter = 0
+    # index is current index, starter is left pointer 
+    # to keep track of the start of the sub string 
+    index = starter = longest = 0
 
-    while index < len(word) - 1:
-        
-        cur_char = word[index]
-        
-        if cur_char in seen:    
-            # update the left side of the window or new starter
-    
-            starter = seen[cur_char]
-            seen[cur_char] = index + 1
-            index = starter
-            print("new index of dub char: ", seen[cur_char])
-            print("starter: ", starter)
-            counter = seen[cur_char] - starter
-        
-        seen[cur_char] = index + 1
-        print('index: ', index)
+    while index < len(word):
+        curr_char = word[index]
+        # check if the char is seen before to slide back the new starter to the left
+        # one after to that dubplicated char
+        if curr_char in seen:
+            starter = max(seen[curr_char], starter)
+         
+        cur_sub_len = index - starter + 1
+        # check if current substring is greater than longest 
+        # and return the longer one
+        longest = max(longest, cur_sub_len)
+
+        # update the position of current char 
+        seen[curr_char] = index + 1
+
+        # slide the window to right
         index += 1
-
-        longest = max(counter, longest)
-        print(seen)
     
     return longest
+
 
 a = 'abcabcbb'
 b = 'dvdf'
 c = 'pwwkew'
-print(optimized_longest_substring(b))
+print(optimized_longest_substring(''))
 
 
-
-
-
-
-def capitalized_words(arr):
-    capitalized = []
-
-    for word in arr:
-        if word[0].isupper():
-            capitalized.append(word)
-
-    return capitalized
-a = ['APp and', 'Python', 'javascpript']
-# print(capitalized_words(a))
 
