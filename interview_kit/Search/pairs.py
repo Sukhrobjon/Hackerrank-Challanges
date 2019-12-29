0e9831bbda8af49cf6afbee7b2d183af98330802
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-12-28 21:55:19 -0800

    [optimized] pairs function using dict

--- interview_kit/Search/pairs.py
+++ interview_kit/Search/pairs.py
@@ -1,4 +1,4 @@
-def pairs(k, numbers):
+def pairs_naive(k, numbers):
     """
     Determine the number of pairs that have difference equal to k
     Args:
@@ -23,8 +23,33 @@ def pairs(k, numbers):
     return count
 
 
-numbers = [1, 5, 3, 4, 2]
+def pairs_optimized(k, numbers):
+    """
+    Determine the number of pairs that have difference equal to k
+    Args:
+        array(int): list of integers
+        k(int): difference between two element needs be found
+    Returns:
+        pairs(int): number of pairs that their difference equal to k
+
+    Note: k is assumed to be positive
+    """
+    # create a dict elements are keys indexes are value
+    num_dict = {k: v for v, k in enumerate(numbers)}
+    count = 0
+    pairs = []
+    for index, num in enumerate(num_dict.keys()):
+        other_num = num + k
+        if other_num in num_dict and num_dict[other_num] != index:
+            count += 1
+            pairs.append((other_num, num))
+    # print(num_dict)
+    return pairs
+
+
+numbers = [1, 5, 3, 4, 2, 2]
 k = 2
-result = pairs(k, numbers)
+# result = pairs(k, numbers)
+result = pairs_optimized(k, numbers)
 print(result)
 
